411d3796d77db82f03339980fbfe8c9964b9bc09
CauePukall <cauegpukall@gmail.com>
2025-03-03 19:34:04 +0000

    1o semestre estacio

--- 1o semestre estacio/Projetos/xadrez2.c
+++ 1o semestre estacio/Projetos/xadrez2.c
@@ -3,17 +3,19 @@
 int main(){
     
     int c1, c;
+    char movimento1[] = "Baixo";
+    char movimento2[] = "Esquerda";
 
     printf("Movimento do Cavalo:\n");
 
     for (c = 0; c < 2; c++) 
     {
-        printf("Baixo\n");
+        printf("%s\n", movimento1);
     }
     
     while (c1 < 1)
     {
-        printf("Esquerda\n");
+        printf("%s\n", movimento2);
         c1++;
     }
 
